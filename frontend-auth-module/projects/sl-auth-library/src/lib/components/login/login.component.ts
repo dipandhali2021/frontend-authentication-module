ce69e77c85b9b79d0472c167f872079af9ebf152
dipandhali2021 <dipandhali2021@gmail.com>
2024-04-15 19:35:44 +0530

    added login page with required validation

--- frontend-auth-module/projects/sl-auth-library/src/lib/components/login/login.component.ts
+++ frontend-auth-module/projects/sl-auth-library/src/lib/components/login/login.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'lib-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  type: string = 'password';
+  display: string = 'visibility_off';
+  isActiveEmail: boolean = false;
+  isActivePass: boolean = false;
 
-  constructor() { }
+  formFields = [
+    { name: 'email', label: 'Email', type: 'email', showIcon: false, icon: '' },
+    {
+      name: 'password',
+      label: 'Password',
+      type: 'password',
+      showIcon: true,
+      icon: 'visibility_off',
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      recaptchaReactive: ['', Validators.required],
+    });
+  }
+  ngOnInit(): void {}
+
+  isFieldInvalid(field: any) {
+    const control = this.loginForm.get(field.name);
+    return control?.invalid && (control.dirty || control.touched);
   }
 
+  setActive(field: any, isActive: boolean) {
+    if (field.name === 'email') {
+      this.isActiveEmail = isActive;
+    } else if (field.name === 'password') {
+      this.isActivePass = isActive;
+    }
+  }
+
+  isActive(field: any) {
+    if (field.name === 'email') {
+      return this.isActiveEmail;
+    } else if (field.name === 'password') {
+      return this.isActivePass;
+    }
+    return false;
+  }
+
+  togglePasswordVisibility(field: any) {
+    field.type = field.type === 'password' ? 'text' : 'password';
+    field.icon = field.type === 'password' ? 'visibility_off' : 'visibility';
+  }
+
+  getFieldErrors(field: any) {
+    const formControl = this.loginForm.get(field.name);
+    const errors = [];
+    if (formControl?.errors) {
+      if (formControl.errors['required']) {
+        errors.push({ message: `${field.label} is required.` });
+      }
+      if (formControl.errors['email']) {
+        errors.push({ message: `Enter a valid ${field.label} ID.` });
+      }
+      if (formControl.errors['minlength']) {
+        errors.push({
+          message: `Please enter minimum 8 characters for ${field.label}.`,
+        });
+      }
+    }
+    return errors;
+  }
+  public onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
--- frontend-auth-module/projects/sl-auth-library/src/lib/sl-auth-library.module.ts
+++ frontend-auth-module/projects/sl-auth-library/src/lib/sl-auth-library.module.ts
@@ -3,7 +3,16 @@ import { SlAuthLibraryComponent } from './sl-auth-library.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { OtpComponent } from './components/otp/otp.component';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecaptchaModule, RecaptchaFormsModule } from 'ng-recaptcha';
 
 
 @NgModule({
@@ -14,9 +23,21 @@ import { OtpComponent } from './components/otp/otp.component';
     OtpComponent
   ],
   imports: [
+    MatCardModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    RecaptchaModule,
+    RecaptchaFormsModule,
   ],
   exports: [
-    SlAuthLibraryComponent
+    SlAuthLibraryComponent,
+    LoginComponent
   ]
 })
 export class SlAuthLibraryModule { }
